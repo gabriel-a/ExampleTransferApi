6f42127d19e1dbe851832cf1cf405b2b6bafa4ad
Gabriel Ajabahian <gajabahian@ripe.net>
2018-06-22 10:47:52 +0200

    Adding transfer types

---
+++ Controllers/TransfersTypeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+using ExampleTransferApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleTransferApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferTypesController : ControllerBase
+    {
+        // GET api/transfertypes
+        [HttpGet]
+        public ActionResult<dynamic> Get()
+        {
+            dynamic wrapper = new ExpandoObject();
+            wrapper.transferTypes = Enum.GetValues(typeof(TransferType)).Cast<TransferType>();;
+            return wrapper;
+        }
+    }
+}
